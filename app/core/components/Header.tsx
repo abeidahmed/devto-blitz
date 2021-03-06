c3023b382ad05c6a8c5c5a88fdf9c431807758c7
abeidahmed <abeidahmed92@gmail.com>
2021-03-06 22:49:25 +0530

    Create Header component

--- app/core/components/Header.tsx
+++ app/core/components/Header.tsx
@@ -1,40 +1,26 @@
 import { Link } from "blitz"
+import { Icon } from "app/core/components/Icon"
 
 function Header() {
   return (
     <header className="px-4 sm:px-6 lg:px-8 bg-white shadow">
       <div className="h-14 flex items-center justify-between max-w-7xl mx-auto">
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center">
           <button
             type="button"
-            className="text-gray-600 focus:outline-none focus:bg-gray-100 hover:text-gray-500 p-2 rounded-md -ml-2"
+            className="mr-2 text-gray-600 focus:outline-none focus:bg-gray-100 hover:text-gray-500 p-2 rounded-md -ml-2 md:hidden"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+            <Icon icon="menu" aria-label="open sidebar" className="w-6 h-6" />
           </button>
           <Link href="/">
-            <a className="inline-block p-2 py-2.5 rounded bg-black leading-5 text-white font-bold uppercase">
+            <a className="inline-block p-2 py-2.5 rounded bg-black leading-5 text-white font-bold uppercase hover:bg-gray-800">
               Dev
             </a>
           </Link>
         </div>
         <div className="space-x-3">
           <Link href="/login">
-            <a className="btn text-indigo-600 bg-white hover:bg-gray-100 focus:ring-indigo-500 focus:ring-opacity-50">
-              Log in
-            </a>
+            <a className="btn btn-invisible">Log in</a>
           </Link>
           <Link href="/signup">
             <a className="btn btn-primary">Create account</a>
