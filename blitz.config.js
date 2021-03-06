54269496bf1c55167cb669d85d825f54476c934b
abeidahmed <abeidahmed92@gmail.com>
2021-03-06 22:01:43 +0530

    Configure sass loader

--- blitz.config.js
+++ blitz.config.js
@@ -1,4 +1,5 @@
 const { sessionMiddleware, simpleRolesIsAuthorized } = require("blitz")
+const path = require("path")
 
 module.exports = {
   middleware: [
@@ -6,6 +7,9 @@ module.exports = {
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
+  sassOptions: {
+    includePaths: [path.join(__dirname, "app", "styles")],
+  },
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
