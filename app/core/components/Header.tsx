4c860190d3e6660e01dd4d754cb3921d4d12a4a6
abeidahmed <abeidahmed92@gmail.com>
2021-03-07 14:19:55 +0530

    Add userDropdown

--- app/core/components/Header.tsx
+++ app/core/components/Header.tsx
@@ -1,43 +1,10 @@
-import { Suspense } from "react"
+import { Suspense, useState } from "react"
 import { Link, useMutation } from "blitz"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import logout from "app/auth/mutations/logout"
 import { Icon } from "app/core/components/Icon"
 
-const UserAction = () => {
-  const currentUser = useCurrentUser()
-  const [logoutMutation] = useMutation(logout)
-
-  if (currentUser) {
-    return (
-      <div>
-        <h5>Logged in</h5>
-        <button
-          type="button"
-          className="button small"
-          onClick={async () => {
-            await logoutMutation()
-          }}
-        >
-          Logout
-        </button>
-      </div>
-    )
-  } else {
-    return (
-      <>
-        <Link href="/login">
-          <a className="btn btn-invisible">Log in</a>
-        </Link>
-        <Link href="/signup">
-          <a className="btn btn-primary">Create account</a>
-        </Link>
-      </>
-    )
-  }
-}
-
-function Header() {
+export default function Header() {
   return (
     <header className="px-4 sm:px-6 lg:px-8 bg-white shadow">
       <div className="h-14 flex items-center justify-between max-w-7xl mx-auto">
@@ -64,4 +31,96 @@ function Header() {
   )
 }
 
-export default Header
+const UserAction = () => {
+  const currentUser = useCurrentUser()
+
+  if (currentUser) {
+    return (
+      <div className="flex items-center">
+        <UserDropdown name={currentUser.name} />
+      </div>
+    )
+  } else {
+    return (
+      <>
+        <Link href="/login">
+          <a className="btn btn-invisible">Log in</a>
+        </Link>
+        <Link href="/signup">
+          <a className="btn btn-primary">Create account</a>
+        </Link>
+      </>
+    )
+  }
+}
+
+const UserDropdown = ({ name }: { name: string | null }) => {
+  const [logoutMutation] = useMutation(logout)
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="relative">
+      <button
+        type="button"
+        onClick={() => setIsOpen(!isOpen)}
+        className="rounded-full focus:outline-none focus:ring-4 focus:ring-indigo-100"
+      >
+        <img
+          className="w-8 h-8 rounded-full"
+          src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+      </button>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } mt-2 absolute right-0 w-64 origin-top-right text-base bg-white shadow-offset-black border-black border-2 rounded`}
+      >
+        <ul className="py-1">
+          <li className="px-1">
+            <a
+              href="#"
+              className="block p-2 focus:outline-none focus:bg-indigo-50 rounded focus:text-indigo-600 hover:bg-indigo-50 hover:text-indigo-600"
+            >
+              <span className="block">{name ? name : "Anonymous"}</span>
+              <span className="block text-sm text-gray-500">@abeidahmed</span>
+            </a>
+          </li>
+          <li className="py-1">
+            <hr />
+          </li>
+          <li className="px-1">
+            <a
+              href="#"
+              className="block p-2 focus:outline-none focus:bg-indigo-50 rounded focus:text-indigo-600 hover:bg-indigo-50 hover:text-indigo-600"
+            >
+              Dashboard
+            </a>
+          </li>
+          <li className="px-1">
+            <a
+              href="#"
+              className="block p-2 focus:outline-none focus:bg-indigo-50 rounded focus:text-indigo-600 hover:bg-indigo-50 hover:text-indigo-600"
+            >
+              Write a post
+            </a>
+          </li>
+          <li className="py-1">
+            <hr />
+          </li>
+          <li className="px-1">
+            <button
+              type="button"
+              className="block w-full text-left p-2 focus:outline-none focus:bg-indigo-50 rounded focus:text-indigo-600 hover:bg-indigo-50 hover:text-indigo-600"
+              onClick={async () => {
+                await logoutMutation()
+              }}
+            >
+              Sign out
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
